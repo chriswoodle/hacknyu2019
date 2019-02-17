a48d6b3450f0bcd6263f60c34a3c1b249b0fc992
Chris Woodle <chris@droplit.io>
2019-02-16 20:57:30 -0500

    Added style, logo and axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/" class="logo">
+        <img alt="Rideable logo" src="./assets/logo.png">
+      </router-link>
+      <router-link to="/">Home</router-link>
+      <span>|</span>
+      <router-link to="/about">About</router-link>
+      <span>|</span>
       <router-link to="/map">Map</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
 
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import axios from "axios";
+
+@Component({
+  components: {}
+})
+export default class App extends Vue {
+  mounted() {
+    console.log(process.env.VUE_APP_SERVICE_HOST);
+    // Hello server!
+    axios({
+      url: process.env.VUE_APP_SERVICE_HOST,
+      method: "get"
+    }).then(response => {
+      console.log(response);
+    });
+  }
+}
+</script>
+
+<style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,13 +46,31 @@
   color: #2c3e50;
 }
 #nav {
+  background-color: #36449f;
   height: 76px;
-  padding: 30px;
+  line-height: 76px;
+  padding-left: 30px;
+  padding-right: 30px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+  span {
+    padding-right: 10px;
+    padding-left: 10px;
+  }
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #ffd300;
+    text-decoration: none;
     &.router-link-exact-active {
-      color: #42b983;
+      color: #ffd300;
+    }
+    &.logo {
+      position: absolute;
+      left: 0;
+      img {
+        padding: 10px;
+        height: 76px;
+
+      }
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -16,3 +15,14 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.home {
+    // background-color: #36449F;
+  img {
+    height: 200px;
+    margin: 20px;
+  }
+}
+</style>
+
